690452b9f3340659c1f33473df558cdb53bc57ce
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2024-03-16 17:39:55 -0300

    feat: adicionar logica do handler editar solicitante

--- handler/requests.go
+++ handler/requests.go
@@ -41,3 +41,20 @@ func (r *CreateSolicitanteRequest) Validate() error {
 
 	return nil
 }
+
+type PatchsolicitanteRequest struct {
+	NomeFantasia string `json:"nome_fantasia"`
+	Cep          string `json:"cep"`
+	Rua          string `json:"rua"`
+	Numero       string `json:"numero"`
+	Cidade       string `json:"cidade"`
+	Estado       string `json:"estado"`
+}
+
+func (r *PatchsolicitanteRequest) Validate() error {
+	if r.NomeFantasia != "" || r.Cep != "" || r.Rua != "" || r.Numero != "" || r.Cidade != "" || r.Estado != "" {
+		return nil
+	}
+
+	return fmt.Errorf("at least one field must be provided")
+}
--- handler/solicitante.go
+++ handler/solicitante.go
@@ -68,7 +68,77 @@ func PostSolicitante(ctx *gin.Context) {
 }
 
 func PatchSolicitante(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PATCH Solicitante",
-	})
+	cnpj := ctx.Query("cnpj")
+
+	if cnpj == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("cnpj", "queryParameter").Error())
+		return
+	}
+
+	request := PatchsolicitanteRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	solicitante := SolicitanteResponse{}
+
+	err := db.QueryRow(context.Background(), "SELECT * FROM solicitante WHERE cnpj = $1", cnpj).Scan(&solicitante.Cnpj, &solicitante.NomeFantasia, &solicitante.Cep, &solicitante.Rua, &solicitante.Numero, &solicitante.Cidade, &solicitante.Estado)
+
+	if err != nil {
+		if err.Error() == "no rows in result set" {
+			sendError(ctx, http.StatusNotFound, "solicitante não encontrado!")
+			return
+		}
+
+		logger.Errorf("querying error: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if request.NomeFantasia != "" {
+		solicitante.NomeFantasia = request.NomeFantasia
+	}
+
+	if request.Cep != "" {
+		solicitante.Cep = request.Cep
+	}
+
+	if request.Rua != "" {
+		solicitante.Rua = request.Rua
+	}
+
+	if request.Cidade != "" {
+		solicitante.Cidade = request.Cidade
+	}
+
+	if request.Estado != "" {
+		solicitante.Estado = request.Estado
+	}
+
+	query := `UPDATE solicitante SET nome_fantasia = @nome_fantasia, cep = @cep, rua = @rua, numero = @numero, cidade = @cidade, estado = @estado WHERE cnpj = @cnpj`
+
+	args := pgx.NamedArgs{
+		"nome_fantasia": solicitante.NomeFantasia,
+		"cep":           solicitante.Cep,
+		"rua":           solicitante.Rua,
+		"numero":        solicitante.Numero,
+		"cidade":        solicitante.Cidade,
+		"estado":        solicitante.Estado,
+		"cnpj":          cnpj,
+	}
+
+	_, err = db.Exec(context.Background(), query, args)
+
+	if err != nil {
+		logger.Errorf("error updating data: %v", err)
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "update-solicitante", "solicitante alterado com sucesso!")
 }
