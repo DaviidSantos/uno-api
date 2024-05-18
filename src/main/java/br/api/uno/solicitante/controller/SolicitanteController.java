8d89ea13b80201f4f09707bf57d6689855996d83
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2024-05-18 14:46:53 -0300

    refactor: separated the exception handlers into a generic class

--- src/main/java/br/api/uno/solicitante/controller/SolicitanteController.java
+++ src/main/java/br/api/uno/solicitante/controller/SolicitanteController.java
@@ -1,21 +1,14 @@
 package br.api.uno.solicitante.controller;
 
 import br.api.uno.solicitante.model.SolicitanteDTO;
-import br.api.uno.solicitante.model.exceptions.CnpjAlreadyRegisteredException;
 import br.api.uno.solicitante.service.SolicitanteService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/solicitante")
@@ -45,36 +38,5 @@ public class SolicitanteController {
     public ResponseEntity<SolicitanteDTO> buscarSolicitantePorCnpj(@RequestParam String cnpj) {
         SolicitanteDTO obj = service.buscarSolicitantePorCnpj(cnpj);
         return ResponseEntity.ok().body(obj);
-    }
-
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler(CnpjAlreadyRegisteredException.class)
-    protected Map<String, String> handleAlreadyRegisteredCnpj(RuntimeException ex, WebRequest request) {
-        Map<String, String> error = new HashMap<>();
-        error.put("erro", ex.getMessage());
-
-        return error;
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(CnpjAlreadyRegisteredException.class)
-    protected Map<String, String> handleSolicitanteNotFound(RuntimeException ex, WebRequest request) {
-        Map<String, String> error = new HashMap<>();
-        error.put("erro", ex.getMessage());
-
-        return error;
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-
-        return errors;
     }
 }
---
+++ src/main/java/br/api/uno/utils/ControllerAdvice.java
@@ -0,0 +1,67 @@
+package br.api.uno.utils;
+
+import br.api.uno.solicitante.model.exceptions.CnpjAlreadyRegisteredException;
+import br.api.uno.solicitante.model.exceptions.SolicitanteNotFoundException;
+import br.api.uno.utils.exceptions.InvalidFieldValueException;
+import br.api.uno.utils.exceptions.MissingFieldException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(CnpjAlreadyRegisteredException.class)
+    protected Map<String, String> handleCnpjAlreadyRegisteredException(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("erro", ex.getMessage());
+
+        return error;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(SolicitanteNotFoundException.class)
+    protected Map<String, String> handleSolicitanteNotFoundException(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("erro", ex.getMessage());
+
+        return error;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingFieldException.class)
+    protected Map<String, String> handleMissingFieldException(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("erro", ex.getMessage());
+
+        return error;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(InvalidFieldValueException.class)
+    protected Map<String, String> handleInvalidFieldValueException(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("erro", ex.getMessage());
+
+        return error;
+    }
+}
